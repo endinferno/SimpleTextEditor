2a86306b1d5b302fb8c5c20d0cec5d31af4b6b71
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-05-28 13:30:42 +0800

    feat: draw row according to the row size of the window

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,19 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define CTRL_KEY(k) ((k) & 0x1F)
 
-struct termios origTermios;
+struct EditorConfig {
+    int screenRow;
+    int screenCol;
+    struct termios origTermios;
+};
+
+struct EditorConfig config;
 
 void die(const char* s)
 {
@@ -19,19 +26,19 @@ void die(const char* s)
 
 void DisableRawMode()
 {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &origTermios) == -1) {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &config.origTermios) == -1) {
         die("tcsetattr");
     }
 }
 
 void EnableRawMode()
 {
-    if (tcgetattr(STDIN_FILENO, &origTermios) == -1) {
+    if (tcgetattr(STDIN_FILENO, &config.origTermios) == -1) {
         die("tcgetattr");
     }
     atexit(DisableRawMode);
 
-    struct termios raw = origTermios;
+    struct termios raw = config.origTermios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -44,9 +51,31 @@ void EnableRawMode()
     }
 }
 
+int GetWindowSize(int& row, int& col)
+{
+    struct winsize ws;
+    if (::ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        row = ws.ws_row;
+        col = ws.ws_col;
+        return 0;
+    }
+}
+
+void EditorDrawRows()
+{
+    for (int y = 0; y < config.screenRow; y++) {
+        ::write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void EditorRefreshScreen()
 {
     ::write(STDOUT_FILENO, "\x1b[2J", 4);
+    ::write(STDOUT_FILENO, "\x1b[H", 3);
+
+    EditorDrawRows();
     ::write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
@@ -76,9 +105,17 @@ void EditorProcessKeyProcess()
     }
 }
 
+void InitEditor()
+{
+    if (GetWindowSize(config.screenRow, config.screenCol) == -1) {
+        die("GetWindowSize");
+    }
+}
+
 int main(int argc, char* argv[])
 {
     EnableRawMode();
+    InitEditor();
 
     while (true) {
         EditorRefreshScreen();
